0bc9759531a8aabb645528787436552f561efe20
Shubham Joshi <joshi.cse17@gmail.com>
2020-07-23 10:54:34 +0530

    23 July 2020

---
+++ binarySearch.py
@@ -0,0 +1,17 @@
+def binarySearchWithRecurssion(list, target, l, r):
+    if l > r:
+        return False
+    mid = (l + r) // 2
+    if list[mid] == target:
+        return True
+    
+    if list[mid] > target:
+        binarySearchWithRecurssion(list, target, l, mid - 1)
+    elif list[mid] < target:
+        binarySearchWithRecurssion(list, target, mid + 1, r)
+    return False
+
+if __name__ == '__main__':
+    l  = [1,2,3,4,5]
+    print(binarySearchWithRecurssion(l, 33, 0, len(l) - 1))
+    
